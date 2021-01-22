afa7a3506c7ea654a5bee6fcf71f754110b3cbbe
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-01-22 19:48:44 +0000

    added html

--- app/models/game.py
+++ app/models/game.py
@@ -1,5 +1,29 @@
-from app.models.player import Player
 
 class Game:
+    
+    
+    def play_game(self, player1_choice, player2_choice):
+        if player1_choice == "rock" and player2_choice =="scissors":
+            result = "Player 1 wins by playing rock"
+            return result
+        elif player1_choice == "scissors" and player2_choice =="rock":
+            result = "Player 2 wins by playing rock"
+            return result
+        elif player1_choice == "paper" and player2_choice =="rock":
+            result = "Player 1 wins by playing paper"
+            return result
+        elif player1_choice == "rock" and player2_choice =="paper":
+            result = "Player 2 wins by playing paper"
+            return result
+        elif player1_choice == "scissors" and player2_choice =="paper":
+            result = "Player 1 wins by playing scissors"
+            return result
+        elif player1_choice == "paper" and player2_choice =="scissors":
+            result = "Player 1 wins by playing scissors"
+            return result
+        elif player1_choice == player2_choice:
+            return none 
 
-    def__init__(self, )
+        
+            
+
--- app/models/player_list.py
+++
@@ -1,5 +0,0 @@
-from app.models.player import Player
-
-player1= Player("John", "Rock")
-player2 = Player("Laura", "Paper")
-player3 = Player("Dave", "Scissors")
