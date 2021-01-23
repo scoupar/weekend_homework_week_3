3b574fc41afaa1fdead5f4da0e02f4bc63ef64dc
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-01-23 20:52:40 +0000

    added css

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,5 +1,14 @@
 from app import app
+from app.models.player import Player
+from app.models.game import Game
+from flask import render_template
 
-@app.route('/')
-def index():
-    return "hello world!"
+
+@app.route('/<choice1>/<choice2>')
+def play(choice1, choice2):
+    player1 = Player("Steve", choice1)
+    player2 = Player("Scott", choice2)
+    game = Game()
+    winner = game.play_game(player1.choice, player1.name, player2.choice, player2.name)
+
+    return render_template('index.html', player1=player1, player2=player2, winner=winner)
--- app/models/game.py
+++ app/models/game.py
@@ -1,28 +1,23 @@
 
+
 class Game:
     
     
-    def play_game(self, player1_choice, player2_choice):
+    def play_game(self, player1_choice, player1_name, player2_choice, player2_name):
         if player1_choice == "rock" and player2_choice =="scissors":
-            result = "Player 1 wins by playing rock"
-            return result
+            return f"{player1_name}wins by playing rock!"
         elif player1_choice == "scissors" and player2_choice =="rock":
-            result = "Player 2 wins by playing rock"
-            return result
+            return "Rock wins!"
         elif player1_choice == "paper" and player2_choice =="rock":
-            result = "Player 1 wins by playing paper"
-            return result
+            return "Paper wins!"
         elif player1_choice == "rock" and player2_choice =="paper":
-            result = "Player 2 wins by playing paper"
-            return result
+            return "Paper wins!"
         elif player1_choice == "scissors" and player2_choice =="paper":
-            result = "Player 1 wins by playing scissors"
-            return result
+            return "Scissors wins!"
         elif player1_choice == "paper" and player2_choice =="scissors":
-            result = "Player 1 wins by playing scissors"
-            return result
+            return "Scissors wins!"
         elif player1_choice == player2_choice:
-            return none 
+            return "It's a draw!"
 
         
             
